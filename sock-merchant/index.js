7aff36a58d6d8f4605cf574fbd6d8d90651b42f5
kerry-okpere <kerryokpere@gmail.com>
2021-03-05 17:30:42 +0100

    feat: find how many pairs are in an array

---
+++ sock-merchant/index.js
@@ -0,0 +1,13 @@
+function sockMerchant(arr, n) {
+  let pair = 0
+  arr = arr.sort()
+  for (let index = 0; index < n; index++) {
+    if(arr[index] === arr[index + 1]){
+      // skip
+      index = index + 1
+      pair = pair + 1
+    }
+  }
+  return pair;
+}
+module.exports = sockMerchant;
